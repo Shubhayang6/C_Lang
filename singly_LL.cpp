533fe08f8dc6f45001239e8917395e56d9f27a3a
Shubhayan Ghosh <subhayang6@gmail.com>
2025-12-10 21:45:48 +0530

    Add singly LL

---
+++ singly_LL.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node
+{
+    int val;
+    struct Node *next;
+
+    Node(int val) // constructor
+    {
+        this->val = val;
+        next = nullptr;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+
+public:
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    void append(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node* temp = head;
+            while(temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    void display()
+    {
+        Node* temp = head;
+        while(temp != nullptr)
+        {
+            cout << temp->val << "->";
+            temp = temp->next;
+        }
+        cout << "NULL";
+    }
+};
+
+int main()
+{
+    LinkedList LL;
+    int i=0;
+    while(i<=10)
+    {
+        LL.append(i);
+        i++;
+    }
+
+    LL.display();
+    
+    
+    return 0;
+}
